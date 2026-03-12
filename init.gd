66779b5647911c4172d572c0e6625a394aeb9d02
SamiiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2026-03-12 21:48:00 +0100

    fix: Corregida indentacion

--- init.gd
+++ init.gd
@@ -8,11 +8,10 @@ func _ready():
 # Sets up the fullscreen toggle available via keyboard shortcuts (F11 or Alt+Enter) [AND FROM OPTIONS MENU IN THE FUTURE]
 func _fullscreen_toggle():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
-				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			else:
-				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func _input(event):
 	if event is InputEventKey and event.pressed and (event.keycode == KEY_F11 or (event.keycode == KEY_ENTER and event.alt_pressed)):
 		_fullscreen_toggle()
-
